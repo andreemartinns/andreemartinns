b4ea3cfdc4c119ceaeeaedd9a7d8d8e1084177d2
andreemartinns <andreemartinns2@gmail.com>
2025-09-04 09:45:18 -0300

    Add files via upload

---
+++ exercicio_while/while.js
@@ -0,0 +1,27 @@
+// count = 0 
+
+// while (count <= 5 ) {
+//   //  count = count + 1 
+//   count++
+
+//     console.log(`White rodando e o count esta em ${count}`)
+// }
+
+// let contador = 5 
+
+// while (contador >= 0) {
+//     if (contador ==0){
+//         console.log("Feliz ano novo")
+//     } else {
+//         console.log(contador)
+//     }
+//     contador--
+// }
+
+let numero = 2 
+let contador = 0
+
+while (contador <= 10) {
+    console.log(`${numero}x ${contador} ${numero} * ${contador}`)
+    contador++
+}
